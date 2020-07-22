209f6c4e90b016522950aa4cfa50e7e61743b800
tyagita <tiagita@gmail.com>
2020-07-22 23:47:41 +0700

    Updates

---
+++ precache-manifest.22ba9904e3ecde6742270668c0e2dd12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a267ad817763700155f9ea0fa1da0e80",
+    "url": "/advotics-test/index.html"
+  },
+  {
+    "revision": "5725174cc2649df5a719",
+    "url": "/advotics-test/static/css/2.2a4cbf1c.chunk.css"
+  },
+  {
+    "revision": "72407dfd942b430533a0",
+    "url": "/advotics-test/static/css/main.e074300d.chunk.css"
+  },
+  {
+    "revision": "5725174cc2649df5a719",
+    "url": "/advotics-test/static/js/2.57b568d8.chunk.js"
+  },
+  {
+    "revision": "0d094d6fc51cec54694436b7d97d069a",
+    "url": "/advotics-test/static/js/2.57b568d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72407dfd942b430533a0",
+    "url": "/advotics-test/static/js/main.a6f59dc3.chunk.js"
+  },
+  {
+    "revision": "39d4f5e4dd5f960b72d1",
+    "url": "/advotics-test/static/js/runtime-main.e48f24b6.js"
+  },
+  {
+    "revision": "da5d8ae55fa005ddb2479351440adb79",
+    "url": "/advotics-test/static/media/sales-turnover.da5d8ae5.svg"
+  }
+]);
